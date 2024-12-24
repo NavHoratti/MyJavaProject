901fead86c67f7683ba1085967c64268a6c7397a
Naveen Horatti <navidon2@gmail.com>
2024-12-24 16:24:31 -0500

    My initial commit

---
+++ Mycode.java
@@ -0,0 +1,22 @@
+package myjavaproject;
+
+
+public class Mycode {
+
+    String animal = "Dog";
+    int age = 3;
+    int x = 20; 
+    int y = 3;
+    
+    public static void main(String[] args){
+
+        System.out.println("My code is here");
+
+        System.out.println("My code is Simple");
+
+        Mycode mycode = new Mycode();
+
+        System.out.println("I have a " + mycode.animal + " who is " + mycode.age + " of age");
+     }
+    
+}
---
+++ SumCalculator.java
@@ -0,0 +1,30 @@
+package myjavaproject;
+
+import java.util.Scanner;
+
+
+public class SumCalculator {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for the first number
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        // Prompt the user for the second number
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        System.out.println("Hello World");
+
+        // Close the scanner
+        scanner.close();
+    }
+}
